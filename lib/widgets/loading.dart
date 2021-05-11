096edd0f30ce019a67550c9a6e9f54b7f22626d6
sumit <sumit@pop-os.localdomain>
2021-05-11 12:21:07 +0530

    added loading icon

--- lib/widgets/loading.dart
+++ lib/widgets/loading.dart
@@ -5,10 +5,11 @@ class Loading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset(
             'assets/images/loading.gif',
-            height: 500,
+            height: 100,
           )
         ],
       ),
