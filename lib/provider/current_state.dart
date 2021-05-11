5bdf03b85d4b9de8ece91ff5f46010a1e01c8a91
sumit <sumit@pop-os.localdomain>
2021-05-11 11:15:30 +0530

    added current state for pin and date management

---
+++ lib/provider/current_state.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CurrentState {
+  int pin;
+  DateTime dateTime;
+  bool isLoading;
+
+  CurrentState(this.pin, this.dateTime, this.isLoading);
+}
+
+class CurrentStateProvider with ChangeNotifier {
+  CurrentState currentState = CurrentState(0, DateTime.now(), false);
+
+  void setDate(DateTime dateTime) {
+    currentState.dateTime = dateTime;
+    notifyListeners();
+  }
+
+  void setPincode(int pincode) {
+    currentState.pin = pincode;
+    notifyListeners();
+  }
+
+  void toggleLoading() {
+    currentState.isLoading = !currentState.isLoading;
+    notifyListeners();
+  }
+
+  DateTime get dateTime {
+    return currentState.dateTime;
+  }
+
+  int get pin {
+    return currentState.pin;
+  }
+}
